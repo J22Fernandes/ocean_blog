c96738beb51e551423290a581e4ba13b712ade9f
J22Fernandes <jjplfernandes@gmail.com>
2022-08-21 22:14:50 +0000

    Criação de posts mocados

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
 from flask import Flask, render_template
+from datetime import datetime
 
-app = Flask ("hello")
-nomeAula ="Aula python para Web"
-
+app = Flask("hello")
+posts = [
+{
+    "title": "Meu primeiro post",
+    "body": "Esse post é o primeiro do blog",
+    "author": "Danilo de Souza Miguel",
+    "created": datetime(2022,8,17)
+},
+{
+    "title": " Meu segundo post",
+    "body": "Esse post é o segundo do blog",
+    "author": "Raiane Caroline",
+    "created": datetime(2022,8,17)
+},
+{
+    "title": " Meu terceiro post",
+    "body": "Esse post é o terceito do blog",
+    "author": "Jezimiel Marcondes",
+    "created": datetime(2022,8,17)
+},
+]
 @app.route("/")
-def hello():
-    return ("Hello World")
-
-@app.route("/usuario")
-def usuario():
-    usuario = [1, "Danilo de Souza Miguel", "Professor"]
-    alunos = ["Andre Guedes", "Lucas Santos", "Alicia Duarte", "Raiane Caroline"]
-    return render_template ("index.html", usuario=usuario, nome=nomeAula, alunos=alunos)
-
-@app.route("/contactos")
-def contacto():
-    return render_template ("index.html", usuario=usuario, nome=nomeAula)
+def index():
+    return render_template("index.html", post=posts)
